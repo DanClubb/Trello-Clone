f256bdbd1900eedec8f1541feebf73da03ed5c6b
daniel-clubb <danclubb004@gmail.com>
2024-01-05 16:55:02 +0000

    created header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        lightgray: '#9fadbc29',
+        darkgray: '#1d2125',
+        lightblue: '#bfdbfe',
+        skyblue: '#579dff'
+      }
+      
     },
   },
   plugins: [],
