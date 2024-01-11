14b79b82d6cd97a4ec33a7c1eaf39b997249c2d4
daniel-clubb <danclubb004@gmail.com>
2024-01-11 17:48:14 +0000

    remove page for being signed out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Boards from "./_components/Boards";
 import DashboardNav from "./_components/DashboardNav";
 
 export default async function Home() {
-  const session = await getServerAuthSession();
+  // const session = await getServerAuthSession();
 
-  if (!session) return <main className="text-center">Not logged in</main>
+  // if (!session) return <main className="text-center">Not logged in</main>
   return (
     <main className="flex justify-center gap-14 px-32 pt-11 grow">
       <DashboardNav />
