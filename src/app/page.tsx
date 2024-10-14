0d78df514b15f3959e000dbb4f38f6b1031d50d5
daniel-clubb <danclubb004@gmail.com>
2024-10-14 15:36:49 +0100

    centered sign in text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import DashboardNav from "./_components/DashboardNav";
 export default async function Home() {
   const session = await getServerAuthSession();
 
-  if (!session) return <main className="text-center">Sign In</main>
+  if (!session) return <main className="flex justify-center items-center text-center">Sign In</main>
   return (
     <main className="flex justify-center gap-14 px-32 pt-11 grow">
       <DashboardNav />
