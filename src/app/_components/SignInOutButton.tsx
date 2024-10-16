7b791d31c7941ef34032bc02e9a945b1f39cc619
daniel-clubb <danclubb004@gmail.com>
2024-10-16 13:13:40 +0100

    changed styles of sign in button

--- src/app/_components/SignInOutButton.tsx
+++ src/app/_components/SignInOutButton.tsx
@@ -9,5 +9,5 @@ interface SignInOutButtonProps {
 export default function SignInOutButton({session}: SignInOutButtonProps) {
   if (session) return <button className="ml-auto py-1 px-2 rounded border border-current text-nowrap transition hover:text-skyblue" onClick={() => signOut()}>Sign out</button>
   
-  return <button className="ml-auto text-nowrap rounded border border-current transition hover:text-skyblue" onClick={() => signIn()}>Sign in</button>
+  return <button className="ml-auto text-nowrap py-1 px-2  rounded border border-current transition hover:text-skyblue" onClick={() => signIn()}>Sign in</button>
 }
